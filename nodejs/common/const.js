71df19e1c54c39dbeb878465db9add1c01229d07
yaoyunchou <303630573@qq.com>
2017-07-28 07:46:43 +0800

    update

--- nodejs/common/const.js
+++ nodejs/common/const.js
@@ -1,6 +1,6 @@
 module.exports = {
-  //API_SERVER: 'http://localhost:8090/api/',
-  API_SERVER: 'https://xfywechat.applinzi.com/api/',
+  API_SERVER: 'http://localhost:8090/api/',
+  //API_SERVER: 'https://xfywechat.applinzi.com/api/',
   //API_SERVER: 'https://wxgjprerelease.nsw99.com/wx/',  
   isRegistered: false,
   user: {},
